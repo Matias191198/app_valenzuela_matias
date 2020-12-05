fa2018d0eba36140ee17a7573a30eb64dbdeff22
juan <navea.juanj@gmail.com>
2020-12-04 21:54:38 -0300

    evaluacion_dos

---
+++ app/src/main/java/com/example/app_valenzuela/SQLiteOpenHelpers.java
@@ -0,0 +1,21 @@
+package com.example.app_valenzuela;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLiteOpenHelpers extends SQLiteOpenHelper{
+    public SQLiteOpenHelpers(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase BaseDeDatos) {
+        BaseDeDatos.execSQL("create table tarjeta(titular text primary key, apell text, num_tar int, mes int, anio int, cod int, derecedit text, ciudad text, estado text, codpos int)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
